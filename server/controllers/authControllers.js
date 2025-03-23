b66631a737859fd44509f6f8efed3ef014a70e02
tahanaveed107 <tahanaveed.107@gmail.com>
2025-03-23 13:30:15 +0500

    Login and Logout controller

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken"
 import bcrypt from "bcryptjs"
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv"
 
 dotenv.config();
@@ -12,9 +12,9 @@ export const registerUser = async (req, res) => {
         
         const {name, email, password} = req.body;
     
-        const existinguser = await prisma.user.findUnique({where:{email: email}});
+        const existingUser = await prisma.user.findUnique({where:{email: email}});
     
-        if (existinguser){
+        if (existingUser){
             return res.status(400).json({
                 message: "User Already Exists"
             })
@@ -33,6 +33,54 @@ export const registerUser = async (req, res) => {
         res.status(201).json({ message: "User Registered Successfully", user: newUser });
     } catch (error) {
         console.error("Error in registerUser:", error);
+        res.status(500).json({ 
+            message: "Something went wrong",
+            error: error.message });
+    }
+}
+
+export const loginUser = async(req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        const existingUser = await prisma.user.findUnique({where: {email}});
+        if (!existingUser) {
+            return res.status(400).json({
+                message: "Invalid Credentials"
+            })
+        }
+
+        const passMatch = bcrypt.compareSync(password, existingUser.password)
+        if (!passMatch) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        const token = jwt.sign(
+            { userId: existingUser.id, email: existingUser.email },
+            process.env.JWT_SECRET,
+            { expiresIn: "1h" }
+        );
+
+        res.cookies("token", token , {
+            httpOnly:true,
+            secure: process.env.NODE_ENV || "production",
+            sameSite: "strict",
+            maxAge:  60*60*1000,  
+        })
+        res.status(200).json({ message: "Login successful" });
+    } catch (error) {
+        console.error("Error in loginUser:", error);
         res.status(500).json({ message: "Something went wrong", error: error.message });
     }
 }
+
+export const logoutUser = (req, res) => {
+    res.cookie("auth_token", "", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 0, // Instantly expire the cookie
+    });
+
+    res.status(200).json({ message: "Logout successful" });
+};
